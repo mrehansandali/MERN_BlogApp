3d76441c56085a3fa2df17a80a3e8fe26b75e10d
Rehan <mrehansandali@gmail.com>
2025-12-31 23:15:49 +0500

    add middleware to the protected routes

--- backend/src/routes/blog.routes.js
+++ backend/src/routes/blog.routes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import { createBlog, deleteBlog, getAllBlogs, getblogBySlug, updateBlog } from "../controllers/blog.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+// public routes
 router.get('/', getAllBlogs);
 router.get('/:slug', getblogBySlug);
-router.post('/', createBlog);
-router.put('/:id', updateBlog);
-router.delete('/:id', deleteBlog);
+
+// protected routes
+router.post('/', protect, createBlog);
+router.put('/:id', protect, updateBlog);
+router.delete('/:id', protect, deleteBlog);
 
 export default router;
